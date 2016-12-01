9c9a0d10c5c196eddcdb3752e07c5c0a5c335690
Evg <evg.tray@gmail.com>
2016-12-01 18:55:18 +0300

    Unique name routes

--- db/migrate/20161130163709_create_routes.rb
+++ db/migrate/20161130163709_create_routes.rb
@@ -1,7 +1,7 @@
 class CreateRoutes < ActiveRecord::Migration[5.0]
   def change
     create_table :routes do |t|
-      t.string :name
+      t.string :name, unique: true
 
       t.timestamps
     end
