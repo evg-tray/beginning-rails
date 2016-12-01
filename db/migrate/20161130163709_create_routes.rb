006e9cd87d573c0ea0c13e1e8b80a3cd53af8489
Evg <evg.tray@gmail.com>
2016-12-01 19:40:03 +0300

    Refactoring routes migration

--- db/migrate/20161130163709_create_routes.rb
+++ db/migrate/20161130163709_create_routes.rb
@@ -1,10 +1,9 @@
 class CreateRoutes < ActiveRecord::Migration[5.0]
   def change
     create_table :routes do |t|
-      t.string :name, unique: true
+      t.string :name, index:true, unique: true
 
       t.timestamps
     end
-    add_index :routes, :name
   end
 end
